aadcfc4bee7db58e2d43346c87576883bb9d6c29
Jay Coskey <jay.coskey@gmail.com>
2021-02-05 15:59:42 -0800

    Remove dead code from previous approach

--- states_viz.py
+++ states_viz.py
@@ -48,42 +48,17 @@ def read_graph(nodes_file, edges_file, order='xy'):
 
 
 def write_dotfile_planned(g, dotfile_name):
-    xs = [attrs['x'] for name, attrs in g.nodes(data=True)]
-    ys = [attrs['y'] for name, attrs in g.nodes(data=True)]
-
-    xmax = int(max(xs))
-    xmin = int(min(xs))
-    ymax = int(max(ys))
-    ymin = int(min(ys))
-
-    coords2state = { (attrs['x'], attrs['y']) : name
-                     for name, attrs in sorted(g.nodes(data=True))
-                   }
-    state2coords = { name : (attrs['x'], attrs['y'])
-                     for name, attrs in sorted(g.nodes(data=True))
-                   }
-
-    def get_name_isstate(x, y):
-        if (x, y) in coords2state:
-            return (coords2state[(x, y)], True)
-        else:
-            return (f'dummy_{x}_{y}', False)
-
     def write_nodes(f):
-        for x in range(xmin, xmax + 1):
-            cluster_name = f'cluster_{x}'
-            f.write('  subgraph ')
-            f.write(cluster_name)
-            f.write('  {\n')
-            f.write('    style=invis\n')
-            for y in range(ymin, ymax + 1):
-                scale_factor = 100
-                (name, is_state) = get_name_isstate(x, y)
-                pos_spec = f'pos="{scale_factor * x:.1f},{scale_factor * y:.1f}"'
-                vis_spec = '' if is_state else 'style=invis'
-                node_spec = f'    {name} [{pos_spec} {vis_spec}]\n'
-                f.write(node_spec)
-            f.write('  }\n')
+        f.write('  node [fontsize=10, width=0.3]\n')
+        f.write('  subgraph cluster_states {\n')
+        for name, attrs in g.nodes(data=True):
+            scale_factor = 50
+            x = attrs['x'] 
+            y = attrs['y'] 
+            pos_spec = f'pos="{scale_factor * x:.1f},{scale_factor * y:.1f}"'
+            node_spec = f'    {name} [{pos_spec}]\n'
+            f.write(node_spec)
+        f.write('  }\n')
 
     def write_edges(f):
         for a, b in sorted(g.edges(data=False)):
@@ -94,25 +69,8 @@ def write_dotfile_planned(g, dotfile_name):
                 annotation = f'[{color} {style}]'
                 f.write(f'  {a} -- {b} {annotation}\n')
 
-#        f.write(f'\n')
-#        for x in range(xmin, xmax):
-#            for y in range(ymin, ymax):
-#                (xy, is_xy_state) = get_name_isstate(x, y)
-#                (east, is_east_state) = get_name_isstate(x + 1, y)
-#                (north, is_north_state) = get_name_isstate(x, y + 1)
-#
-#                if not (is_xy_state and is_east_state):
-#                    f.write(f'  {xy:12s} -- {east:12s}  [style=invis, weight=1000]\n')
-#                if not (is_xy_state and is_north_state):
-#                    f.write(f'  {xy:12s} -- {north:12s}  [style=invis, weight=1000]\n')
-#                f.write('\n')
-
     with open(dotfile_name, 'w') as f:
         f.write('strict graph States {\n')
-        # f.write('  rankdir=LR\n')
-        # f.write('  node [style="rounded,filled"];\n')
-        # f.write('  graph [style=filled, splines=line];\n')
-        # f.write('  rank=same;\n')
         f.write('\n')
         write_nodes(f)
         f.write('\n')
@@ -129,7 +87,6 @@ def write_dotfile_springs(g, dotfile_name):
 
     def write_edges(f):
         f.write('    AK -- HI [style=invis]\n\n')  # Appear nearby, but unlinked
-        # f.write('    HI -- SC [style=invis]\n\n')  # Custom tweak to move AK, HI
         f.write('    NJ -- CT [style=invis]\n')  # Custom tweak to move NJ
         f.write('    NJ -- MD [style=invis]\n\n')  # Custom tweak to move NJ
         for a, b in sorted(g.edges(data=False)):
