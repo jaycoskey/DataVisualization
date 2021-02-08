3b9897bc3503cac170ddcf0ad7da4995a30fb13c
Jay Coskey <jay.coskey@gmail.com>
2021-02-08 00:54:21 -0800

    Move AK & HI from the southeast to the southwest

--- states_viz.py
+++ states_viz.py
@@ -5,34 +5,39 @@ import networkx as nx
 import sys
 
 
-STATES_COORDS_FILE         = 'states.coords'
-STATES_EDGES_FILE          = 'states.edges'
-STATES_LATLONG_FILE        = 'states.latlong'
+GRAY_ATTRS = 'fillcolor=lightgray style=filled'
 
+STATES_CONN_NODE  = [('AZ','CO'), ('NM','UT')]
+STATES_WEST_COAST = ['WA', 'OR', 'CA']
 
-STATES_DOTFILE_PLANNED_OUT = 'states_planned.dot'
-STATES_DOTFILE_SPRINGS_OUT = 'states_springs.dot'
+STATES_DOTFILE_PLANNED = 'states_planned.dot'
+STATES_DOTFILE_SPRINGS = 'states_springs.dot'
+
+STATES_FILE_COORDS  = 'states.coords'
+STATES_FILE_EDGES   = 'states.edges'
+STATES_FILE_LATLONG = 'states.latlong'
 
 
 def read_graph(nodes_file, edges_file, order='xy'):
     def read_nodes(nodes_file):
         with open(nodes_file, 'r') as f:
             for line in f.readlines():
-                if line.startswith('#'): continue
-                items = line.strip().split()
+                items = line.strip().split('#')[0].split()
+                if len(items) == 0:
+                    continue
                 assert(len(items) == 3)
-                name = items[0]
-                x = float(items[1])
-                y = float(items[2])
+                name, x, y = items
                 if order == 'yx':
                     x, y = y, x
-                yield (name, x, y)
+                yield (name, float(x), float(y))
 
     def read_edges(edges_file):
         with open(edges_file, 'r') as f:
             for line in f.readlines():
-                if line.startswith('#'): continue
-                vals = line.strip().split(':')
+                vals = line.strip().split('#')[0].split(':')
+                if len(vals) == 0:
+                    continue
+                assert(len(vals) == 2)
                 name = vals[0]
                 nbrs = vals[1].split() if len(vals) > 1 else []
                 yield (name, nbrs)
@@ -49,60 +54,79 @@ def read_graph(nodes_file, edges_file, order='xy'):
 
 def write_dotfile_planned(g, dotfile_name):
     def write_nodes(f):
-        f.write('  node [fontsize=10, width=0.3]\n')
-        f.write('  subgraph cluster_states {\n')
-        for name, attrs in g.nodes(data=True):
+        # writeln(f, 1, 'subgraph cluster_states {')
+        for name, attrs in sorted(g.nodes(data=True)):
             scale_factor = 50
-            x = attrs['x'] 
-            y = attrs['y'] 
+            x = attrs['x']
+            y = attrs['y']
             pos_spec = f'pos="{scale_factor * x:.1f},{scale_factor * y:.1f}"'
-            node_spec = f'    {name} [{pos_spec}]\n'
-            f.write(node_spec)
-        f.write('  }\n')
+            writeln(f, 1, f'{name} [{pos_spec}]')
+        # writeln(f, 1, '}')
 
     def write_edges(f):
         for a, b in sorted(g.edges(data=False)):
             if b > a:
-                is4 = (a, b) in [('AZ','CO'), ('NM','UT')]
-                color = 'color=red'    if is4 else ''
-                style = 'style=dashed' if is4 else ''
-                annotation = f'[{color} {style}]'
-                f.write(f'  {a} -- {b} {annotation}\n')
+                is_conn_node = (a, b) in STATES_CONN_NODE
+                color = 'color=red'
+                style = 'style=dashed'
+                attrs = f'[{color} {style}]' if is_conn_node else ''
+                writeln(f, 1, f'{a} -- {b} {attrs}')
 
     with open(dotfile_name, 'w') as f:
-        f.write('strict graph States {\n')
-        f.write('\n')
+        writeln(f, 0, 'strict graph States {')
+        writeln(f, 1, 'node [fixedsize=true fontsize=10 height=0.45 width=0.45]')
+        writeln(f)
         write_nodes(f)
-        f.write('\n')
+        writeln(f)
         write_edges(f)
-        f.write('}\n')
+        writeln(f, 0, '}')
 
 
 def write_dotfile_springs(g, dotfile_name):
+    name2attrs = {name:attrs for name,attrs in g.nodes(data=True)}
+
     def write_nodes(f):
         for name, attrs in sorted(g.nodes(data=True)):
-            is48 = name not in ['AK', 'HI']
-            pos_attrs = f'[pos="{attrs["x"]:.2f},{attrs["y"]:.2f}"]' if is48 else ''
-            f.write(f'    {name} {pos_attrs}\n')
+            if name == 'AK':
+                x = name2attrs['CA']['x']
+                y = name2attrs['TX']['y']
+            elif name == 'HI':
+                x = name2attrs['NM']['x']
+                y = name2attrs['TX']['y']
+            else:
+                x = attrs['x']
+                y = attrs['y']
+            pos_attrs = f'[pos="{x:.2f},{y:.2f}"]'
+            writeln(f, 1, f'{name} {pos_attrs}')
 
     def write_edges(f):
-        f.write('    AK -- HI [style=invis]\n\n')  # Appear nearby, but unlinked
-        f.write('    NJ -- CT [style=invis]\n')  # Custom tweak to move NJ
-        f.write('    NJ -- MD [style=invis]\n\n')  # Custom tweak to move NJ
+        writeln(f, 1, 'AK -- HI [style=invis]')  # Place nearby, but unlinked
+        writeln(f, 1, 'AK -- CA [style=invis]')  # Tweak to place AK near CA
+        writeln(f, 1, 'HI -- TX [style=invis]')  # Tweak to place HI near TX
+        writeln(f, 1, 'NJ -- CT [style=invis]')  # Tweak to place NJ
+        writeln(f, 1, 'NJ -- MD [style=invis]')  # Tweak to place NJ
+        writeln(f)
         for a, b in sorted(g.edges(data=False)):
             if b > a:  # Avoid duplication
-                is4 = (a, b) in [('AZ','CO'), ('NM','UT')]
+                is4 = (a, b) in STATES_CONN_NODE
                 edge_attrs = '[color=red style=dashed]' if is4 else ''
-                f.write(f'    {a} -- {b} {edge_attrs}\n')
+                writeln(f, 1, f'{a} -- {b} {edge_attrs}')
 
     with open(dotfile_name, 'w') as f:
-        f.write('strict graph States {\n')
-        f.write('    rankdir=LR\n')
-        f.write('    splines=line\n\n')
+        writeln(f, 0, 'strict graph States {')
+        writeln(f, 1, 'rankdir=LR')
+        writeln(f, 1, 'splines=line')
+        writeln(f, 1, 'node [fixedsize=true fontsize=10 height=0.45 width=0.45]')
+        writeln(f)
         write_nodes(f)
-        f.write('\n')
+        writeln(f)
         write_edges(f)
-        f.write('}\n')
+        writeln(f, 0, '}')
+
+
+def writeln(f, indent_level=0, s=''):
+    f.write(' ' * 4 * indent_level)
+    f.write(s + '\n')
 
 # ----------------------------------------
 
@@ -117,22 +141,22 @@ def test_graph(state_graph):
 # ----------------------------------------
 
 def make_dotfile_planned():
-    src = read_graph( nodes_file=STATES_COORDS_FILE
-                    , edges_file=STATES_EDGES_FILE
+    src = read_graph( nodes_file=STATES_FILE_COORDS
+                    , edges_file=STATES_FILE_EDGES
                     )
     test_graph(src)
     write_dotfile_planned( g=src
-                         , dotfile_name=STATES_DOTFILE_PLANNED_OUT
+                         , dotfile_name=STATES_DOTFILE_PLANNED
                          )
 
 
 def make_dotfile_springs():
-    src = read_graph( nodes_file=STATES_LATLONG_FILE
-                    , edges_file=STATES_EDGES_FILE
+    src = read_graph( nodes_file=STATES_FILE_LATLONG
+                    , edges_file=STATES_FILE_EDGES
                     , order='yx'
                     )
     test_graph(src)
-    write_dotfile_springs(g=src, dotfile_name=STATES_DOTFILE_SPRINGS_OUT)
+    write_dotfile_springs(g=src, dotfile_name=STATES_DOTFILE_SPRINGS)
 
 
 if __name__ == '__main__':
